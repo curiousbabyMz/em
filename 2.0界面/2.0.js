a828acc70285a08e841032378cbfc7b09cf37427
curiousbabyMz <285532050@qq.com>
2017-12-18 18:44:17 +0800

    officechange for emax

---
+++ 2.0界面/2.0.js
@@ -0,0 +1,69 @@
+function cardAction(a) {
+    let t = "1500ms ";
+    let m1 = "cardin ";
+    let m2 = "infoin ";
+    let card = a.nextElementSibling;
+    let lpic = a.nextElementSibling.nextElementSibling;
+    let ff = a.parentNode.parentNode;
+
+    card.style.animation = m1 + t + "forwards";
+    lpic.style.animation = m2 + t + "forwards";
+    for (let i = 0; i < ff.childNodes.length; i++) {
+        if (ff.childNodes[i].tagName == "DIV" && ff.childNodes[i] !== a.parentNode) {
+            ff.childNodes[i].childNodes[3].style.animation = "elseout 1s forwards";
+        }
+    }
+}
+
+function cardAction2(a) {
+    let t = "1500ms ";
+    let m1 = "cardout ";
+    let m2 = "infoout ";
+    let card = a.nextElementSibling;
+    let lpic = a.nextElementSibling.nextElementSibling;
+    let ff = a.parentNode.parentNode;
+
+    card.style.animation = m1 + t + "forwards";
+    lpic.style.animation = m2 + t + "forwards";
+    for (let i = 0; i < ff.childNodes.length; i++) {
+        if (ff.childNodes[i].tagName == "DIV" && ff.childNodes[i] !== a.parentNode) {
+            ff.childNodes[i].childNodes[3].style.animation = "elsein 1s forwards";
+        }
+    }
+}
+
+function start(a) {
+    let t = "1800";
+    a.firstElementChild.removeAttribute("onmouseenter");
+    a.firstElementChild.removeAttribute("onmouseleave");
+    a.firstElementChild.nextElementSibling.style.animation = "start " + t + "ms";
+    a.lastElementChild.previousElementSibling.style.animation = "infoout 1.5s forwards";
+    let ff = a.parentNode;
+    for (let i = 0; i < ff.childNodes.length; i++) {
+        if (ff.childNodes[i].tagName == "DIV" && ff.childNodes[i] !== a) {
+            ff.childNodes[i].childNodes[3].style.animation = "elsein 1s forwards";
+        }
+    }
+    setTimeout(reset, t);
+
+    function reset() {
+        a.firstElementChild.setAttribute("onmouseenter", "cardAction(this)");
+        a.firstElementChild.setAttribute("onmouseleave", "cardAction2(this)");
+
+    }
+
+}
+
+function nextpage() {
+    var allpage = document.getElementsByClassName("items");
+    for (let i = 0; i < allpage.length; i++) {
+        if (allpage[i].style.display != "none") {
+            allpage[i].style.animation = "npageout 2s forwards";
+            setTimeout(function () {
+                allpage[i + 1].style.display="";
+                allpage[i + 1].style.animation = "npagein 2s forwards";
+            }, 1000);
+            console.log(allpage[i]);
+        }
+    }
+}
